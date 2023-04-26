82127b8f07a1ae2c79fafc64d94de4081d47e3b1
Jaanus Kase <jaanus@gmail.com>
2023-04-26 13:53:13 +0300

    Initial Commit

---
+++ PackageStringsBug/PackageStringsBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageStringsBugApp.swift
+//  PackageStringsBug
+//
+//  Created by Jaanus Kase on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PackageStringsBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
